2f86130079a157987e1669f3dc81c9018b986f29
Wilmer Arambula <terabytesoftw@gmail.com>
2025-07-21 11:56:07 -0400

    refactor(Response): Change base class from Yii\web\Response to BaseResponse for improved compatibility.

--- src/http/Response.php
+++ src/http/Response.php
@@ -7,9 +7,10 @@ namespace yii2\extensions\psrbridge\http;
 use Psr\Http\Message\{ResponseFactoryInterface, ResponseInterface, StreamFactoryInterface};
 use Yii;
 use yii\web\Cookie;
+use yii\web\Response as BaseResponse;
 use yii2\extensions\psrbridge\adapter\ResponseAdapter;
 
-final class Response extends Yii\web\Response
+final class Response extends BaseResponse
 {
     public function getPsr7Response(): ResponseInterface
     {
