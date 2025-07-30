00edf558db1242aa94c2b8ff4ae786f379f7822c
Wilmer Arambula <terabytesoftw@gmail.com>
2025-07-30 05:48:27 -0400

    test: Add test for handling request errors in `StatelessApplication` class.

--- tests/adapter/ServerRequestAdapterTest.php
+++ tests/adapter/ServerRequestAdapterTest.php
@@ -1824,4 +1824,43 @@ final class ServerRequestAdapterTest extends TestCase
             'User-Agent header should NOT be filtered as it is not a secure header.',
         );
     }
+
+    /**
+     * @throws InvalidConfigException
+     */
+    public function testThrowableOccursDuringRequestHandling(): void
+    {
+        $_SERVER = [
+            'REQUEST_METHOD' => 'GET',
+            'REQUEST_URI' => 'nonexistent/invalidaction',
+        ];
+
+        $request = FactoryHelper::createServerRequestCreator()->createFromGlobals();
+
+        $app = $this->statelessApplication();
+
+        $response = $app->handle($request);
+
+        self::assertSame(
+            404,
+            $response->getStatusCode(),
+            "Response status code should be '404' when handling a request to 'non-existent' route in " .
+            "'StatelessApplication', confirming proper error handling in catch block.",
+        );
+        self::assertSame(
+            'text/html; charset=UTF-8',
+            $response->getHeaders()['content-type'][0] ?? '',
+            "Response 'content-type' should be 'text/html; charset=UTF-8' for error response when 'Throwable' occurs " .
+            "during request handling in 'StatelessApplication'.",
+        );
+
+        $body = $response->getBody()->getContents();
+
+        self::assertStringContainsString(
+            '<pre>Not Found: Page not found.</pre>',
+            $body,
+            "Response body should contain error message about 'Not Found: Page not found' when 'Throwable' occurs " .
+            "during request handling in 'StatelessApplication'.",
+        );
+    }
 }
