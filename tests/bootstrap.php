1fed202bc7e44dedd04d6e92f1f4092f6287fd30
Wilmer Arambula <terabytesoftw@gmail.com>
2025-08-01 12:59:26 -0400

    refactor(tests): Remove unnecessary server variable definitions in `bootstrap.php` file.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,6 +10,3 @@ define('YII_ENV', 'test');
 // require composer autoloader if available
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
-
-$_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
-$_SERVER['SCRIPT_FILENAME'] = __FILE__;
