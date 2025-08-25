ef9ec0b50ec5706eb3a310e46e7a36bcce224d6c
Wilmer Arambula <terabytesoftw@gmail.com>
2025-08-25 10:48:24 -0400

    test(http): Extract content types from `ApplicationTest` to `ApplicationContentTypesTest` class.

---
+++ tests/http/stateless/ApplicationContentTypesTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace yii2\extensions\psrbridge\tests\http\stateless;
+
+use yii\base\InvalidConfigException;
+use yii2\extensions\psrbridge\tests\support\FactoryHelper;
+use yii2\extensions\psrbridge\tests\TestCase;
+
+final class ApplicationContentTypesTest extends TestCase
+{
+    /**
+     * @throws InvalidConfigException if the configuration is invalid or incomplete.
+     */
+    public function testFileDownloadResponse(): void
+    {
+        $app = $this->statelessApplication();
+
+        $response = $app->handle(FactoryHelper::createRequest('GET', '/site/file'));
+
+        self::assertSame(
+            200,
+            $response->getStatusCode(),
+            "Expected HTTP '200' for route 'site/file'.",
+        );
+        self::assertSame(
+            'text/plain',
+            $response->getHeaderLine('Content-Type'),
+            "Expected Content-Type 'text/plain' for route 'site/file'.",
+        );
+        self::assertSame(
+            'This is a test file content.',
+            $response->getBody()->getContents(),
+            "Expected response 'body' to match plain text 'This is a test file content.'.",
+        );
+        self::assertSame(
+            'attachment; filename="testfile.txt"',
+            $response->getHeaderLine('Content-Disposition'),
+            "Expected Content-Disposition 'attachment; filename=\"testfile.txt\"'.",
+        );
+    }
+}
