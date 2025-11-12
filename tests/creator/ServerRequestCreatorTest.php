da611af9b26c38d34ba3710b90850a800353de13
Wilmer Arambula <terabytesoftw@gmail.com>
2025-11-12 07:09:23 -0300

    fix(tests): Remove unused PHPStan ignore comments from test models.

--- tests/creator/ServerRequestCreatorTest.php
+++ tests/creator/ServerRequestCreatorTest.php
@@ -110,9 +110,6 @@ final class ServerRequestCreatorTest extends TestCase
                 throw new RuntimeException('Stream creation failed.');
             }
 
-            /**
-             * @phpstan-ignore throws.unusedType
-             */
             public function createStreamFromFile(string $filename, string $mode = 'r'): StreamInterface
             {
                 throw new RuntimeException('Stream creation failed.');
